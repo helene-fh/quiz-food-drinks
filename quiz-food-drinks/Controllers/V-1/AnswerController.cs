8772f75a4cf45a35f8c680e20203d5fe2b82a8d8
Vincent-STAT <Vincent.pilstrom.tipmung@iths.se>
2023-01-15 04:55:15 +0100

    answercontroller,swagger doc

--- quiz-food-drinks/Controllers/V-1/AnswerController.cs
+++ quiz-food-drinks/Controllers/V-1/AnswerController.cs
@@ -28,16 +28,17 @@ namespace quiz_food_drinks.Controllers
         /// <summary>
         /// Add a new answer to a Question(id)
         /// </summary>
-        /// <param name="answer"></param>
         /// <returns>A new added Answer to a Question.</returns>
         /// <remarks>
         ///**Sample request:**
-        /// ```Answer
+        /// ```
+        /// POST /Todo
         /// {
         ///     "questionId": "Enter an id from a Question!",
         ///     "answerText": "It´s Superman",
         ///     "isCorrectAnswer": true
-        /// }``
+        /// }
+        /// ```
         ///</remarks>
         /// <response code="200"></response>
         /// <response code="404"></response>
@@ -62,27 +63,32 @@ namespace quiz_food_drinks.Controllers
         /// <remarks>
         /// **Sample request:**
         ///```
+        ///GET
         ///[
         /// {
-        ///
         ///"questionId": "6658a09f-5532-4e28-951f-637dfe5f66c1",
         /// "answerText": "Yolo",
         /// "isCorrectAnswer": false,
         /// "id": "333dfc00-9a56-42b9-b1fa-678e0e50f3b5"
-        ///
+        /// },
+        /// {
+        ///"questionId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        /// "answerText": "Me",
+        /// "isCorrectAnswer": true,
+        /// "id": "333dfc00-9a56-42b9-b1fa-678e0e50f3b5"
         /// }
         ///]
         ///```
         /// </remarks>
-        /// <response code="200">Returns the list of answers</response>
-        /// <response code="400">Whops something went wrong</response>
+        /// <response code="200">Returns the list of all answers</response>
+        /// <response code="400">Bad request!</response>
         [HttpGet(Name ="GetAllAnswers")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Question>> GetAnswers()
+        public async Task<ActionResult<Answer>> GetAnswers()
         {
             var answer = await _answerService.AllAnswers();
-            if (answer==null) { return BadRequest("try again later!"); }
+            if (answer==null) { return BadRequest(); }
             return Ok(answer);
         }
 
@@ -152,7 +158,7 @@ namespace quiz_food_drinks.Controllers
         /// <summary>
         /// Delete an answer
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">Answer id to be deleted!</param>
         /// <returns>deleted answer</returns>
         /// <remarks>
         ///**Sample request:**
@@ -165,7 +171,7 @@ namespace quiz_food_drinks.Controllers
     ///
     /// </remarks>
     /// <response code="200">Deleted answer</response>
-    /// /// <response code="404">Invalid id</response>
+    /// <response code="404">Invalid id</response>
     [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
