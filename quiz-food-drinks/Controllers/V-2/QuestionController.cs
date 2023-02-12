98caa82558d23653b5efa3a324a9b37e01bd33f5
Vincent-STAT <Vincent.pilstrom.tipmung@iths.se>
2023-02-12 14:48:42 +0100

    Squashed commit of the following:
    commit 6f9f7b2511b6c14648a66380dac4540667d437d4
    Author: Vincent-STAT <Vincent.pilstrom.tipmung@iths.se>
    Date:   Sun Feb 12 14:40:36 2023 +0100
    
        added swagger documentation
    
    commit 9dbe773804fae7e20dddd8c05407d287b8e45638
    Author: Vinc93 <91186316+Vinc93@users.noreply.github.com>
    Date:   Sun Feb 5 23:34:45 2023 +0100
    
        updated logic in quiz service
    
    commit 4205a3f716df666bdf60cfd56ba175452f7d0da8
    Author: Vincent-STAT <Vincent.pilstrom.tipmung@iths.se>
    Date:   Tue Jan 17 12:17:29 2023 +0100
    
        small updates
    
    commit 1ebac5eb385352f0c02c57663194ea3cf1986454
    Author: Vincent-STAT <Vincent.pilstrom.tipmung@iths.se>
    Date:   Sun Jan 15 21:18:26 2023 +0100
    
        some updates for quizcontroller
    
    commit 8772f75a4cf45a35f8c680e20203d5fe2b82a8d8
    Author: Vincent-STAT <Vincent.pilstrom.tipmung@iths.se>
    Date:   Sun Jan 15 04:55:15 2023 +0100
    
        answercontroller,swagger doc
    
    commit 6721bc8d82b44d9346ab2d7fed0326f1fc44cb0f
    Author: Vinc93 <91186316+Vinc93@users.noreply.github.com>
    Date:   Thu Jan 12 10:07:29 2023 +0100
    
        more swagger doc/ impemented checkanswer
    
    commit 7a9b7355f50ca211a810f62f17936f66e42ee944
    Author: Vinc93 <91186316+Vinc93@users.noreply.github.com>
    Date:   Thu Dec 29 04:30:33 2022 +0100
    
        added quiz CheckAnswer
    
    commit d3abbb425c1ae6ebe859b685daf70b58e2fec498
    Author: Vinc93 <91186316+Vinc93@users.noreply.github.com>
    Date:   Fri Dec 23 13:02:37 2022 +0100
    
        swagger doc v1 up

---
+++ quiz-food-drinks/Controllers/V-2/QuestionController.cs
@@ -0,0 +1,229 @@
+using Microsoft.AspNetCore.Mvc;
+using quiz_food_drinks.Entities;
+using quiz_food_drinks.Interfaces.Services;
+using quiz_food_drinks.Persistance;
+using quiz_food_drinks.Services;
+using quiz_food_drinks.ViewModels.Question.cs;
+
+namespace quiz_food_drinks.Controllers;
+[ApiController]
+[Route("api/question_Trivia/[controller]")]
+[ApiExplorerSettings(GroupName = "question_Trivia")]
+[Produces("application/json")]
+
+public class QuestionController : ControllerBase
+{
+   private readonly IQuestionService _questionService;
+    
+    public QuestionController(IQuestionService questionService)
+    {
+        _questionService = questionService;
+    }
+
+    /// <summary>
+    /// Add a question
+    /// </summary>
+    /// <returns>A added question</returns>
+    /// <remarks>
+    /// **Sample request:**
+    ///     
+    ///```
+    /// POST /Todo
+    /// {
+    ///     "questionText": "string",
+    ///     "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ///     "category": "string"
+    /// }
+    /// ```
+    /// </remarks>
+    /// <response code="200">Success, added a new question</response>
+    /// <response code="404">Error 404, try again later?</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Question>> AddQuestion(QuestionCreateRequest question)
+    {
+        if (question is null)
+        {
+            return NotFound("Invalid input");
+        }
+
+        return Ok(await _questionService.AddQuestion(question));
+    }
+
+
+
+
+
+    /// <summary>
+    /// Get all Questions!
+    /// </summary>
+    /// <returns>All Questions</returns>
+    /// <remarks>
+    /// **Sample request:**
+    /// ```
+    /// GET
+    /// [
+    /// {
+    ///   "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ///   "questionText": "Some random question",
+    ///   "category": "Some category"
+    /// },
+    /// {
+    ///   "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ///   "questionText": "Some random question",
+    ///   "category": "Some category"
+    /// },
+    /// ]
+    /// ```
+    /// </remarks>
+    /// <response code="200">Get all Questions!</response>
+    /// <response code="400">Bad request!</response>
+    [HttpGet(Name ="GetAllQuestions")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Question>> GetQuestions()
+    {
+        var question = await _questionService.AllQuestions();
+        if (question!=null) {return Ok(question); }
+        return BadRequest();
+    }
+
+    /// <summary>
+    /// Get question by id
+    /// </summary>
+    /// <returns>A question</returns>
+    /// <remarks>
+    /// **Sample request:**
+    ///   ```
+    ///     GET
+    ///     {
+    ///   "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ///   "questionText": "Some random question",
+    ///   "category": "Some category"
+    ///     }
+    ///```
+    /// </remarks>
+    /// <response code="200">You get the question</response>
+    /// <response code="404">Invalid id</response>
+    /// <param name="id">Question id(Guid) to found question</param>
+    [HttpGet("{id}",Name ="GetQuestionById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Question>> GetQuestion(Guid id)
+    {
+        var question = await _questionService.GetQuestion(id);
+        
+        if (question is null)
+        {
+            return NotFound("Invalid id");
+        }
+        return Ok(question);
+    }
+
+    /// <summary>
+    /// Get a random question
+    /// </summary>
+    /// <returns>Randomed question</returns>
+    /// <remarks>
+    /// **Sample request:**
+    ///     
+    /// ```
+    /// GET
+    /// {
+    ///     "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ///     "questionText": "Who counts as a sith-lord?",
+    ///     "category": "Star Wars"
+    /// }
+    /// ```
+    /// </remarks>
+    /// <response code="200">Success, gets a random question</response>
+    /// <response code="404">Failed, Not Found try again!</response>
+    [HttpGet]
+    [Route("api/[controller]/Random")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Question>> GetRandomQuestion()
+    {
+        var question = await _questionService.GetRandomQuestion();
+        if (question == null) { return NotFound("E404,Not Found"); }
+        return Ok(question);
+
+    }
+
+
+    /// <summary>
+    /// Update a question by id
+    /// </summary>
+    /// <returns>Updated question</returns>
+    /// <remarks>
+    /// **Sample request:**
+    ///     
+    ///   ```
+    ///   PUT /Todo
+    ///   {
+    ///     "questionText": "string",
+    ///     "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ///     "category": "string",
+    ///     "questionId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ///   }
+    ///   ```
+    ///</remarks>
+    /// <response code="200">Success, updated question</response>
+    /// <response code="400">Error 400, check inputs and id(Guid)</response>
+    /// <param name="id">id(Guid) of the Question to update</param>
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Question>> UpdateQuestion(QuestionUpdateRequest question, Guid id)
+    {
+        if (id != question.QuestionId)
+        {
+            return BadRequest("Invalid input");
+        }
+
+        await _questionService.GetQuestion(id);
+        
+        if (question.QuestionId == id)
+        {
+            await _questionService.UpdateQuestion(question);
+        }
+        return Ok(question);
+    }
+
+    
+
+    /// <summary>
+    /// Delete by id
+    /// </summary>
+    /// <returns>Deleted question</returns>
+    /// <remarks>
+    /// **Sample request:**
+    ///```
+    /// DELETE /Todo
+    /// {
+    /// "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    /// "questionText": "Who counts as a sith-lord?",
+    /// "category": "Star Wars"
+    /// }
+    /// ```
+    ///</remarks>
+    /// <response code="200">Success, deleted the question</response>
+    /// <response code="404">Invalid id</response>
+    /// <param name="id">Question id(Guid) to delete question</param>
+[HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Question>> DeleteQuestion(Guid id)
+    {
+        var question = await _questionService.DeleteQuestion(id);
+
+        if (question is null)
+        {
+            return NotFound("Invalid id");
+        }
+
+        return Ok(question);
+    }
+
+}
--- quiz-food-drinks/Services/QuizService.cs
+++ quiz-food-drinks/Services/QuizService.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using quiz_food_drinks.Entities;
 using quiz_food_drinks.Interfaces.Repositories;
 using quiz_food_drinks.Interfaces.Services;
@@ -13,6 +16,9 @@ public class QuizService : IQuizService
     private readonly IQuestionService _questionService;
     private readonly IAnswerService _answerService;
     private readonly ITriviaRepository _triviaRepository;
+    private static List<Answer>? shuffledAnswersList;
+    private QuizModel? trivia;
+    private QuizModel quiz;
 
     public QuizService(IQuestionService questionService, IAnswerService answerService, ITriviaRepository triviaRepository)
     {
@@ -20,24 +26,50 @@ public class QuizService : IQuizService
         _answerService = answerService;
         _triviaRepository = triviaRepository;
     }
-    
+
     public async Task<QuizModel?> GetRandomQuiz()
     {
         Random random = new Random();
-        int source = random.Next(2);
-        
-        if (source == 0)
+        var quizList = new List<object?>
         {
-           return await GetSingleTrivia();
-        }
-        
-        var dbQuiz = await GetQuizFromDb();
-        if (dbQuiz != null)
+            true,
+            false
+        };
+        var randomsQuiz = quizList.ElementAt(random.Next(0, quizList.Count));
+        if (randomsQuiz.Equals(true))
         {
-            return dbQuiz;
+            trivia = await GetSingleTrivia();
+            return trivia as QuizModel;
         }
-        
-        return await GetSingleTrivia();
+        else
+        {
+            quiz = await GetQuizFromDb();
+            return quiz as QuizModel;
+        }
+
+       // trivia = await GetSingleTrivia();
+       // quiz = await GetQuizFromDb();
+        //var triviaId = trivia.Id;
+        // var questionId = question.Id;
+
+        // LINQ som går in Questions och ser om triviaId finns
+
+        //Addera question i databas
+        //var newQuestion = new QuestionCreateRequest();
+        //await _questionService.AddQuestion(newQuestion);
+
+        // En lista för random  bara
+        //var quizList = new List<object?>
+        //{
+        //    trivia,
+        //    quiz
+        //};
+
+        // Slumpa vilken som ska exponeras utåt
+        //Random random = new Random();
+        //var randomQuiz = quizList.ElementAt(random.Next(0, quizList.Count));
+
+        //return randomQuiz as QuizModel;
     }
 
 
@@ -67,15 +99,50 @@ public class QuizService : IQuizService
                     Answers = new List<string>(),
                 };
 
-                await AddRandomizedAnswersList(quizModel, question);
+              //  await _questionService.AddQuestion();
+
+                var filteredAnswers = await _answerService.Get(question.Id);
+                //Random random = new Random();
+                shuffledAnswersList = filteredAnswers.ToList();
+                Debug.WriteLine("!!!!!!!!!!!!!");
+                for (int i = 0; i < shuffledAnswersList.Count(); i++)
+                {
+
+                    Debug.WriteLine(shuffledAnswersList[i].AnswerText + " " + shuffledAnswersList[i].IsCorrectAnswer);
+                }
+                Debug.WriteLine("!!!!!!!!!!!!!");
+                foreach (var answer in filteredAnswers)
+                {
+                    var counterN = 1;
+                    if (answer != null)
+                    {
+                        Console.WriteLine($"{answer.AnswerText}");
+                        Console.WriteLine($"{answer.QuestionId}");
+                        quizModel.Answers.Add($"{counterN}. "+answer.AnswerText);
+                    }
+                    counterN++;
+                }
+
                 return quizModel;
             }
-            
-            SaveTriviaQuestion(response[0], triviaId);
-            SaveCorrectTriviaAnswers(quiz, triviaId);
-            SaveIncorrectTriviaAnswers(quiz.IncorrectAnswers, triviaId);
-            var newQuestion = await _questionService.GetQuestion(triviaId);
-            if (newQuestion != null)
+
+            var triviaQuestion = new QuestionCreateRequest();
+            triviaQuestion.QuestionText = quiz.Question;
+            triviaQuestion.Category = quiz.Category;
+            triviaQuestion.Id = triviaId;
+
+            //Console.WriteLine($"1111111111111111111111 {triviaQuestion.QuestionText}{triviaId}");
+            await _questionService.AddQuestion(triviaQuestion);
+
+            var correctAnswer = new AnswerCreateRequest();
+            correctAnswer.AnswerText = quiz.CorrectAnswer;
+            correctAnswer.IsCorrectAnswer = true;
+            correctAnswer.QuestionId = triviaId;
+            //Console.WriteLine($"1111111111111111111111 {correctAnswer.AnswerText}{correctAnswer.QuestionId}");
+            await _answerService.AddAnswer(correctAnswer);
+            //Console.WriteLine($"1111111111111111111111 {correctAnswer.AnswerText}{correctAnswer.QuestionId}");
+
+            foreach (var a in quiz.IncorrectAnswers)
             {
                 quizModel = new QuizModel
                 {
@@ -91,10 +158,9 @@ public class QuizService : IQuizService
             return null;
         }
 
-        return quizModel;
-        
+        return responseQuiz;
     }
-    
+
 
     private async Task<Question?> CheckTriviaQuestionInDb(Question? question)
     {
@@ -107,7 +173,7 @@ public class QuizService : IQuizService
                 Category = question.Category,
                 Answers = new List<string>(),
             };
-            
+
             await AddRandomizedAnswersList(quizModel, question);
         }
 
@@ -145,7 +211,7 @@ public class QuizService : IQuizService
     private async Task<QuizModel?> GetQuizFromDb()
     {
         var responseQuestion = await _questionService.GetRandomQuestion();
-        
+
         if (responseQuestion is null)
         {
             return null;
@@ -158,13 +224,35 @@ public class QuizService : IQuizService
             Category = responseQuestion.Category,
             Answers = new List<string>(),
         };
-        
-        await AddRandomizedAnswersList(responseQuiz, responseQuestion);
+
+        Console.WriteLine($"{responseQuestion.Id}");
+        // Hämtar alla svar med samma questionId som question från Answers
+        var filteredAnswers = await _answerService.Get(responseQuestion.Id);
+        Random random = new Random();
+         shuffledAnswersList = filteredAnswers.OrderBy(_ => random.Next()).ToList();
+        Debug.WriteLine("!!!!!!!!!!!!!");
+        for (int b = 0; b < shuffledAnswersList.Count(); b++)
+        {
+
+            Debug.WriteLine(shuffledAnswersList[b].AnswerText + " " + shuffledAnswersList[b].IsCorrectAnswer);
+        }
+        Debug.WriteLine("!!!!!!!!!!!!!");
+        var counterN = 1;
+        foreach (var answer in shuffledAnswersList)
+        {
+            if (answer != null)
+            {
+                Console.WriteLine($"{answer.AnswerText}");
+                Console.WriteLine($"{answer.QuestionId}");
+                responseQuiz.Answers.Add($"{counterN}. "+answer.AnswerText);
+            }
+            counterN++;
+        }
 
         return responseQuiz;
     }
-    
-    public async Task<bool> CheckTriviaQuestionIdInDb(Guid id)
+
+    /*public async Task<QuizModel?> GetAnswersFromDb()
     {
         return await _questionService.QuestionExists(id);
     }
@@ -174,16 +262,37 @@ public class QuizService : IQuizService
         Random random = new Random();
         var filteredList = await _answerService.Get(responseQuestion.Id);
         var shuffledAnswersList = filteredList.OrderBy(_ => random.Next()).ToList();
-        
-         foreach (var answer in shuffledAnswersList) 
+
+         foreach (var answer in shuffledAnswersList)
          {
              if (answer != null)
              {
                  responseQuiz.Answers.Add(answer.AnswerText);
-             } 
+             }
          }
 
-         return shuffledAnswersList;
-    }
+        return responseQuiz;
+    }*/
 
+
+
+    public  ActionResult<string>? getTrue(int input)
+    {
+        /* Debug.WriteLine("!!!!!!!!!!!!!");
+         for (int i = 0; i < shuffledAnswersList.Count(); i++)
+         {
+
+             Debug.WriteLine(shuffledAnswersList[i].AnswerText + " " + shuffledAnswersList[i].IsCorrectAnswer);
+         }
+         Debug.WriteLine("!!!!!!!!!!!!!");
+        */
+        if (shuffledAnswersList==null) { return null; }
+        if (input <= 0) { return "PLs dont type 0! One of the answers numbers!"; }
+        if (input > shuffledAnswersList.Count()) { return "Pls choose one of the answers listed by the question!"; }
+
+        if (shuffledAnswersList[input-1].IsCorrectAnswer.Equals(true)) { return $"You choose {shuffledAnswersList[input-1].AnswerText}, You got it right!"; }
+
+        return $"You choose {shuffledAnswersList[input-1].AnswerText},Sry wrong answer!";
+
+    }
 }
